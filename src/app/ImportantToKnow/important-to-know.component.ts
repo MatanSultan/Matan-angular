02784aa697dacb510b712936f3bffae52784c5ed
Matan sultan <88273616+MatanSultan@users.noreply.github.com>
2022-03-25 12:42:17 +0300

    Add files via upload

---
+++ src/app/ImportantToKnow/important-to-know.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: "important-to-know",
+  templateUrl: 'important-to-know.component.html',
+  styleUrls: ["important-to-know.component.scss"]
+})
+export class ImportantToKnowComponent implements OnInit {
+
+
+  ngOnInit() {
+
+  }
+
+
+}
---
+++ src/app/model/Todo.ts
@@ -0,0 +1,12 @@
+
+
+export interface Todo {
+    id: number;
+    description: string;
+
+}
+
+
+
+
+
